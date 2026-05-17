cfe033ccc4689c05a838bd6116c5371871dbfc05
Ziya Üre <ziyaure06@gmail.com>
2026-05-18 00:00:24 +0300

    fix

--- Assets/Scripts/Player/WeaponDecorator.cs
+++ Assets/Scripts/Player/WeaponDecorator.cs
@@ -1,4 +1,4 @@
-using UnityEngine;
+ï»¿using UnityEngine;
 
 public abstract class WeaponDecorator : IWeapon
 {
@@ -9,8 +9,6 @@ public abstract class WeaponDecorator : IWeapon
         _decoratedWeapon = weaponToDecorate;
     }
 
-    // Arayüzden gelen FireRate'i virtual yaparak alt sýnýflarýn (RapidFire gibi) 
-    // onu ezmesine (override) izin veriyoruz.
     public virtual float FireRate => _decoratedWeapon.FireRate;
 
     public virtual void Fire(Transform firePoint, Vector3 direction)
